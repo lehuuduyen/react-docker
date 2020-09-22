31ef01529ac017240f3bd6ac002a95247ad01f91
lehuuduyen lehuuduyen <lehuuduyen96@gmail.com>
2020-09-22 14:55:36 +0700

    up master

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          le huu duyen
+          Learn React
         </a>
       </header>
     </div>
